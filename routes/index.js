84383323825b151ee2ba3e7e8a56b6e6f9f0bb7a
Theran Meadows <theranmeadows@Therans-MacBook-Air.local>
2022-09-14 21:55:23 -0700

    testing GET request

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
+const userController = require('../controller/user');
 
-routes.get('/', (req, res) => {
-    res.send('Stephanie Meadows');
-});
+// routes.get('/', (req, res) => {
+//     res.send('Stephanie Meadows');
+// });
+
+routes.get('/', userController.getUser);
+routes.get('/username', userController.getUsername);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
+const mongodb = require('./db/conect');
 const port = 3000
 
 app.use('/', require('./routes'))
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
