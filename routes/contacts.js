b0ce2c7dda39c8590c4d47363da08450729d695b
Theran Meadows <theranmeadows@Therans-MacBook-Air.local>
2022-09-17 10:24:50 -0700

    L02 Assignement

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const contactsController = require('../controllers/contacts');
+const contactsController = require('../controllers/user');
 
-router.get('/', contactsController.getAll);
+router.get('/', contactsController.getUser);
 
-router.get('/:id', contactsController.getSingle);
+router.get('/:id', contactsController.getUsername);
 
 module.exports = router;
